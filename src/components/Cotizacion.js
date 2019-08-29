092306f5098f389e755cf22772bcde7b419496eb
Juanjo <juanjofb86@gmail.com>
2019-08-29 17:32:53 +0200

    Proyecto terminado

--- src/components/Cotizacion.js
+++ src/components/Cotizacion.js
@@ -8,6 +8,12 @@ const Cotizacion = ({ resultado }) => {
     return (
         <div className="resultado">
             <h2>Resultado</h2>
+            <p className="precio">El precio es <span>{resultado.PRICE}</span></p>
+
+            <p>El precio más alto del dia {resultado.HIGHDAY}</p>
+            <p>El precio más bajo del dia {resultado.LOWDAY}</p>
+            <p>Variación ultimas 24 horas {resultado.CHANGEPCT24HOUR}</p>
+            <p>Ultima actualización {resultado.LASTUPDATE}</p>
         </div>
     );
 }
